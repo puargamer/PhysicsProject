97b0387a1c2610714741746720387eaf64021437
puargamer <ijkwok@ucsc.edu>
2023-05-19 01:37:29 -0700

    added basic character movement

--- game.js
+++ game.js
@@ -1,8 +1,21 @@
+//
+//PREFABS
+//
 class Player extends Phaser.Physics.Arcade.Sprite {
-    constructor(scene) {
-        super(scene);
+    constructor(scene, x, y) {
+        super(scene, x, y, 'player');
+
+        let image = scene.add.image(x, y, 'player');
+
         
-        let body = scene.add.rectangle(650,375,250,10,0x3c78d8);
+    }
+}
+
+class Enemy extends Phaser.Physics.Arcade.Sprite {
+    constructor(scene, x, y) {
+        super(scene, x, y, 'enemy');
+
+        let image = scene.add.image(x, y, 'enemy');
     }
 }
 
@@ -15,11 +28,60 @@ class level1 extends Phaser.Scene {
     }
     preload() {
         this.load.image('background', './assets/field.png');
+        this.load.image('player', './assets/player.png');
+        this.load.image('enemy', './assets/enemy.png');
     }
     create() {
+        //keyboard input
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+        //assets
         let background = this.add.image(0,0, 'background').setOrigin(0);
     
-        let noob = this.physics.add.existing(new Player(this));
+        this.noob = this.physics.add.sprite(100, 100, 'player')
+        this.noob.setCollideWorldBounds(true);
+    }
+    update() {
+        //x movement
+        if(this.cursors.left.isDown) { 
+
+            this.noob.angle = 180;
+            this.noob.setVelocityX(-180);
+
+        }
+        else if (this.cursors.right.isDown) {
+            this.noob.angle = 0;
+            this.noob.setVelocityX(180);
+        }
+        else{
+            this.noob.setVelocityX(0);
+        }
+
+        //y movement
+        if(this.cursors.up.isDown) {
+            this.noob.angle = -90;
+            this.noob.setVelocityY(-180);
+
+            //angles character for diagonal movement
+            if (this.cursors.right.isDown) {
+                this.noob.angle = 315}
+            else if (this.cursors.left.isDown) {
+                this.noob.angle = 225}
+        }
+        else if (this.cursors.down.isDown) {
+            this.noob.angle = 90;
+            this.noob.setVelocityY(180);
+
+            //angles character for diagonal movement
+            if (this.cursors.right.isDown) {
+                this.noob.angle = 45}
+            else if (this.cursors.left.isDown) {
+                this.noob.angle = 135}
+        }
+        else{
+            this.noob.setVelocityY(0);
+        }
+        
     }
 }
 
@@ -38,4 +100,4 @@ const game = new Phaser.Game({
     },
     scene: [level1],
     title: "Physics Game",
-});
+})
